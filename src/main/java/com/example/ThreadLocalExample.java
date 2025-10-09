ab8b803aaf523afbd9451db9bc7e5d30c69e93d5
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-09 19:31:01 +0530

    Test violation files

---
+++ src/main/java/com/example/ThreadLocalExample.java
@@ -0,0 +1,17 @@
+package main.java.com.example;
+
+public class ThreadLocalExample {
+
+    private static final ThreadLocal<String> context = new ThreadLocal<>();
+
+    public void process(String val) {
+        context.set(val);
+        try {
+            doWork();
+        } catch (Exception e) {
+        }
+    }
+
+    private void doWork() {
+    }
+}
