d367d1996e01b3da76f2d9eb643718af9d900134
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-10 21:21:49 +0530

    added under try block

--- Factorial.java
+++ Factorial.java
@@ -7,9 +7,10 @@ public class Factorial {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int n = sc.nextInt();
-        System.out.println("Factorial: " + fact(n));
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter number: ");
+            int n = scanner.nextInt();
+            System.out.println("Factorial: " + fact(n));
+        }
     }
 }
